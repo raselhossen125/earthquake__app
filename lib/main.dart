723fdd8b673854216998a754d9bcf9e7fb9e1244
rasel-hossen <straselhossen24@gmail.com>
2022-09-03 01:14:09 +0600

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,20 @@
+// ignore_for_file: prefer_const_constructors, unused_local_variable
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:provider/provider.dart';
 import 'pages/home_page.dart';
 import 'provider/earthquake_provider.dart';
 
 void main() {
+  SystemChrome.setSystemUIOverlayStyle(
+    SystemUiOverlayStyle(
+      statusBarColor: Colors.white,
+      statusBarIconBrightness: Brightness.dark,
+      statusBarBrightness: Brightness.dark,
+    ),
+  );
   runApp(
     ChangeNotifierProvider(
       create: (_) => EarthquakeProvider(),
@@ -18,12 +28,27 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Map<int, Color> pokeballRedSwatch = {
+      50: Color.fromARGB(255, 24, 1, 105),
+      100: Color.fromARGB(255, 255, 88, 88),
+      200: Color.fromARGB(255, 255, 88, 88),
+      300: Color.fromARGB(255, 255, 88, 88),
+      400: Color.fromARGB(255, 255, 88, 88),
+      500: Color.fromARGB(255, 255, 88, 88),
+      600: Color.fromARGB(255, 255, 88, 88),
+      700: Color.fromARGB(255, 255, 88, 88),
+      800: Color.fromARGB(255, 255, 88, 88),
+      900: Color.fromARGB(255, 252, 70, 70),
+    };
+    MaterialColor pokeballRed = MaterialColor(0xff08677D, pokeballRedSwatch);
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.deepOrange,
+        primarySwatch: pokeballRed,
       ),
+      builder: EasyLoading.init(),
       initialRoute: HomePage.routeName,
       routes: {
         HomePage.routeName: (context) => HomePage(),
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,7 @@
-
-// ignore_for_file: use_key_in_widget_constructors, sort_child_properties_last, prefer_const_constructors, unused_element
+// ignore_for_file: use_key_in_widget_constructors, sort_child_properties_last, prefer_const_constructors, unused_element, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 import '../provider/earthquake_provider.dart';
@@ -20,27 +20,15 @@ class _HomePageState extends State<HomePage> {
   String? startDate;
   String? endDate;
   double? mt;
-  List<double> mtValue = [
-    4,5,6,7,8,9,10
-  ];
+  List<double> mtValue = [4, 5, 6, 7, 8, 9, 10];
 
   @override
   void didChangeDependencies() {
     if (isInit) {
       provider = Provider.of<EarthquakeProvider>(context);
-      // _getData();
       isInit = false;
     }
     super.didChangeDependencies();
-  }
-
-  void _getData() {
-    try {
-      provider.setNewValue(st: '2022-08-29', et: '2022-08-30', mt: 5);
-      provider.getEarthquakeData();
-    } catch (error) {
-      rethrow;
-    }
   }
 
   Future<void> showStartDatePickerDialog(BuildContext context) async {
@@ -71,17 +59,37 @@ class _HomePageState extends State<HomePage> {
     }
   }
 
+  void goValueButton() {
+    if (startDate == null) {
+      showMsg(context, 'Please chose start date');
+      return;
+    }
+    if (endDate == null) {
+      showMsg(context, 'Please chose end date');
+      return;
+    }
+    if (mt == null) {
+      showMsg(context, 'Please chose mt value');
+      return;
+    }
+    EasyLoading.show(
+      status: 'Please Wait'
+    );
+    provider.setNewValue(st: startDate!, et: endDate!, mt: mt!);
+    provider.getEarthquakeData();
+    EasyLoading.dismiss();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Earthquake List'),
-      ),
+      backgroundColor: Colors.white,
       body: Consumer<EarthquakeProvider>(
         builder: (context, value, child) => Column(
           children: [
+            SizedBox(height: 60),
             Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+              padding: const EdgeInsets.symmetric(horizontal: 15),
               child: Row(
                 children: [
                   Expanded(
@@ -90,61 +98,95 @@ class _HomePageState extends State<HomePage> {
                         showStartDatePickerDialog(context);
                       },
                       child: Container(
-                        height: 60,
-                        color: Colors.black,
-                        child: Center(child: Text(
-                          startDate == null ?  "No Chossen" : startDate!,
-                          style: TextStyle(color: Colors.white),
-                        ),)
-                      ),
+                          height: 55,
+                          decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(15),
+                              boxShadow: [
+                                BoxShadow(
+                                  blurRadius: 2,
+                                  spreadRadius: 1,
+                                  color: Colors.grey,
+                                )
+                              ]),
+                          child: Center(
+                            child: Text(
+                              startDate == null ? "Start Date" : startDate!,
+                              style: TextStyle(color: Colors.black),
+                            ),
+                          )),
                     ),
                     flex: 2,
                   ),
-                  SizedBox(width: 5),
+                  SizedBox(width: 10),
                   Expanded(
                     child: InkWell(
                       onTap: () {
                         showEndDatePickerDialog(context);
                       },
                       child: Container(
-                        height: 60,
-                        color: Colors.black,
+                        height: 55,
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(15),
+                            boxShadow: [
+                              BoxShadow(
+                                blurRadius: 2,
+                                spreadRadius: 1,
+                                color: Colors.grey,
+                              )
+                            ]),
                         child: Center(
                           child: Text(
-                            endDate == null ? 'no chosen' : endDate!,
-                            style: TextStyle(color: Colors.white),
+                            endDate == null ? 'End Date' : endDate!,
+                            style: TextStyle(color: Colors.black),
                           ),
                         ),
                       ),
                     ),
                     flex: 2,
                   ),
-                  SizedBox(width: 5),
+                  SizedBox(width: 10),
                   Expanded(
                     child: InkWell(
-                      onTap: () {
-
-                      },
+                      onTap: () {},
                       child: Container(
                         alignment: Alignment.center,
-                        height: 60,
-                        color: Colors.red,
+                        height: 55,
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(15),
+                            boxShadow: [
+                              BoxShadow(
+                                blurRadius: 2,
+                                spreadRadius: 1,
+                                color: Colors.grey,
+                              )
+                            ]),
                         child: DropdownButton(
+                          hint: FittedBox(
+                              child: Text(
+                            'No Value ',
+                            style: TextStyle(fontSize: 14),
+                          )),
                           value: mt,
                           underline: Text(""),
                           dropdownColor: Colors.white,
                           icon: Icon(
                             Icons.keyboard_arrow_down,
-                            color: Colors.white,
+                            color: Colors.black,
                           ),
                           style: TextStyle(
                               fontWeight: FontWeight.w400,
-                              color: Colors.white,
+                              color: Colors.black,
                               fontSize: 18),
                           items: mtValue.map((v) {
                             return DropdownMenuItem(
                               value: v,
-                              child: Text(v.toString(), style: TextStyle(color: Colors.black),),
+                              child: Text(
+                                v.toString(),
+                                style: TextStyle(color: Colors.black),
+                              ),
                             );
                           }).toList(),
                           onChanged: (newValue) {
@@ -157,17 +199,28 @@ class _HomePageState extends State<HomePage> {
                     ),
                     flex: 2,
                   ),
-                  SizedBox(width: 5),
+                  SizedBox(width: 10),
                   Expanded(
                     child: InkWell(
-                      onTap: () {
-                        provider.setNewValue(st: startDate!, et: endDate!, mt: mt!);
-                        provider.getEarthquakeData();
-                      },
+                      onTap: goValueButton,
                       child: Container(
-                        height: 60,
-                        color: Colors.black,
-                        child: Center(child: Text('Go', style: TextStyle(color: Colors.white),),),
+                        height: 55,
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(15),
+                            boxShadow: [
+                              BoxShadow(
+                                blurRadius: 2,
+                                spreadRadius: 1,
+                                color: Colors.grey,
+                              )
+                            ]),
+                        child: Center(
+                          child: Text(
+                            'Go',
+                            style: TextStyle(color: Colors.black),
+                          ),
+                        ),
                       ),
                     ),
                     flex: 1,
@@ -175,27 +228,89 @@ class _HomePageState extends State<HomePage> {
                 ],
               ),
             ),
+            SizedBox(height: 10),
             provider.earthquakeModel != null
                 ? Expanded(
-                    child: ListView.builder(
-                        itemCount: provider.earthquakeModel!.features!.length,
-                        itemBuilder: (context, index) {
-                          final data =
-                              provider.earthquakeModel!.features![index];
-                          return Card(
-                            elevation: 5,
-                            child: ListTile(
-                              title: Text(data.properties!.place == null ? 'No Place' : data.properties!.place!),
-                              subtitle: Text(getFormattedDateTime(
-                                  data.properties!.time!,
-                                  'yyyy-MM-dd hh mm a')),
-                              trailing: Text(data.properties!.mag.toString()),
-                            ),
-                          );
-                        }),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 15),
+                      child: ListView.builder(
+                          itemCount: provider.earthquakeModel!.features!.length,
+                          itemBuilder: (context, index) {
+                            final data =
+                                provider.earthquakeModel!.features![index];
+                            return InkWell(
+                              onTap: () {},
+                              child: Container(
+                                  margin: EdgeInsets.only(bottom: 15),
+                                  height: 60,
+                                  color: Colors.white,
+                                  child: Padding(
+                                    padding: const EdgeInsets.only(
+                                        left: 15, top: 0, bottom: 0, right: 10),
+                                    child: Row(
+                                      children: [
+                                        Container(
+                                          alignment: Alignment.center,
+                                          height: 40,
+                                          width: 40,
+                                          decoration: BoxDecoration(
+                                              color: Colors.white,
+                                              borderRadius:
+                                                  BorderRadius.circular(10),
+                                              boxShadow: [
+                                                BoxShadow(
+                                                  blurRadius: 2,
+                                                  spreadRadius: 1,
+                                                  color: Colors.grey,
+                                                )
+                                              ]),
+                                          child: Text(
+                                              data.properties!.mag.toString()),
+                                        ),
+                                        SizedBox(width: 20),
+                                        Column(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.center,
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          children: [
+                                            Wrap(
+                                              children: [
+                                                Text(
+                                                  data.properties!.place == null
+                                                      ? 'No Place'
+                                                      : data.properties!.place!,
+                                                  style: TextStyle(
+                                                    fontWeight: FontWeight.bold,
+                                                    fontSize: 16,
+                                                    overflow:
+                                                        TextOverflow.ellipsis,
+                                                  ),
+                                                  softWrap: false,
+                                                  maxLines: 1,
+                                                ),
+                                              ],
+                                            ),
+                                            SizedBox(height: 5),
+                                            Text(getFormattedDateTime(
+                                                data.properties!.time!,
+                                                'yyyy-MM-dd   hh mm a')),
+                                          ],
+                                        )
+                                      ],
+                                    ),
+                                  )),
+                            );
+                          }),
+                    ),
                   )
-                : Center(
-                    child: Text('No Data Found'),
+                : Column(
+                    children: [
+                      SizedBox(height: 330),
+                      Center(
+                        child: Text('No data found'),
+                      ),
+                    ],
                   ),
           ],
         ),
